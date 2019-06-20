5d99875a28af051709de196099c795ef70a5c51c
zhudj <zhudj@ect888.com>
2019-06-20 09:29:07 +0800

    feat(ocr3服务示例代码)

---
+++ vba-client-demo-pure/src/test/java/com/ect888/func267/Json267.java
@@ -0,0 +1,21 @@
+package com.ect888.func267;
+
+import com.ect888.bean.JsonCommon;
+
+import java.util.ArrayList;
+
+public class Json267 extends JsonCommon{
+
+	private ArrayList<Result267> results;
+
+	public ArrayList<Result267> getResults() {
+		return results;
+	}
+
+	public void setResults(ArrayList<Result267> results) {
+		this.results = results;
+	}
+	
+	
+
+}
---
+++ vba-client-demo-pure/src/test/java/com/ect888/func268/Json268.java
@@ -0,0 +1,21 @@
+package com.ect888.func268;
+
+import com.ect888.bean.JsonCommon;
+
+import java.util.ArrayList;
+
+public class Json268 extends JsonCommon{
+
+	private ArrayList<Result268> results;
+
+	public ArrayList<Result268> getResults() {
+		return results;
+	}
+
+	public void setResults(ArrayList<Result268> results) {
+		this.results = results;
+	}
+	
+	
+
+}
