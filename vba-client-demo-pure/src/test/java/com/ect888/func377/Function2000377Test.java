072caf8e28657741e914af7b3f2805fea0270004
ect888Zhouxx <56865856+zhou-xx@users.noreply.github.com>
2020-07-16 15:09:39 +0800

    Create Function2000377Test.java

---
+++ vba-client-demo-pure/src/test/java/com/ect888/func377/Function2000377Test.java
@@ -0,0 +1,151 @@
+package com.ect888.func377;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+import com.ect888.bus.FunctionCommon;
+import com.ect888.bus.impl.FunctionCommonImpl;
+import com.ect888.config.Config;
+import com.ect888.http.PoolClient;
+
+/**
+ * 出入境身份认证
+ * 
+ */
+public class Function2000377Test {
+	
+	private static Log log = LogFactory.getLog(Function2000377Test.class);
+	
+	static final String FUNC_NO="2000377";
+	
+
+	/**
+	 * 来源渠道，填固定值“0”
+	 * 
+	 * 参与签名
+	 */
+	String sourcechnl="0";
+	/**
+	 * 业务发生地
+	 * 符合入参长度即可，不做技术限制
+	 * 
+	 * 参与签名
+	 */
+	String placeid="00";
+	/**
+	 * 业务类型
+	 * 符合入参长度即可，不做技术限制
+	 * 
+	 * 参与签名
+	 */
+	String biztyp="A001";
+	/**
+	 * 服务描述
+	 * 
+	 * 符合入参长度即可，不做技术限制
+	 * 
+	 * 参与签名
+	 */
+	String biztypdesc="对比服务";
+	/**
+	 * 时间戳
+	 * 
+	 * 参与签名
+	 */
+	String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	
+	private Config config=Config.getInstance();
+	
+	private PoolClient client=PoolClient.getInstance();
+	
+	private FunctionCommonImpl funcCommon=FunctionCommonImpl.getInstance();
+	
+	/**
+	 * 
+	 *
+	 * @return 将入参，按照http post上送和签名规则，放入map内
+	 */
+	private Map<String, String> buildParams()  throws Exception{
+		Map<String,String> params=new HashMap<String,String>();
+
+		params.put(FunctionCommon.TO_SIGN_HEAD+"timestamp", timestamp);
+		params.put(FunctionCommon.TO_SIGN_HEAD+"biztypdesc", biztypdesc);
+		params.put(FunctionCommon.TO_SIGN_HEAD+"biztyp", biztyp);
+		params.put(FunctionCommon.TO_SIGN_HEAD+"placeid", placeid);
+		params.put(FunctionCommon.TO_SIGN_HEAD+"sourcechnl", sourcechnl);
+		params.put(FunctionCommon.TO_SIGN_HEAD+"ptyacct",config.getPtyacct());
+		params.put(FunctionCommon.TO_SIGN_HEAD+"ptycd",config.getPtycd());	
+		
+		
+		params.put(FunctionCommon.TO_SIGN_HEAD+"idType","553");
+		params.put(FunctionCommon.TO_SIGN_HEAD+"nation","FRA");
+		params.put(FunctionCommon.TO_AES_TO_URL_TO_BASE64_HEAD+"idNumber", "FRA120080010202");
+		params.put(FunctionCommon.TO_SIGN_HEAD+"name","ZHANGSANSAN");
+		
+
+		params.put("funcNo", FUNC_NO);
+		
+		return params;
+	}
+	
+	/**
+	 * 模拟调用
+	 */
+	public void doWork()  throws Exception{
+			
+		Map<String, String> params=buildParams();
+		//加密加签,发起post请求，UrlEncodedFormEntity方式，选择相信服务端ssl证书，忽略证书认证
+		String result = funcCommon.invoke(params);
+		log.info("result=>"+result);
+		//解析返回数据并处理
+		processResult(result);
+	}
+	
+	/**
+	 * json结果result的解析并处理
+	 * 
+	 * @param result
+	 */
+	private void processResult(String result) {
+		 Json377 json=JSON.parseObject(result, Json377.class);
+		
+		 if("0".equals(json.getError_no())) {//系统级调用成功
+			 if(json.getResults().isEmpty()||null==json.getResults().get(0))//异常，系统级调用成功，却无结果，健壮性考虑，留此分支,联系服务端
+				 throw new IllegalStateException("异常，系统级调用成功，却无结果，健壮性考虑，留此分支,联系服务端");
+			 
+			 Result377 re=json.getResults().get(0);
+			 log.info("订单成功结束");
+			 }else {//异常，未知返回码，健壮性考虑，留此分支,联系服务端
+				 throw new IllegalStateException("异常，未知返回码,联系服务端");
+			 }
+		 }
+		
+	
+	@Test
+	public void test() throws Exception{
+		try {
+			doWork();
+		}catch(RuntimeException e){
+			log.error("运行时异常",e);
+		}finally {
+			//应用结束，关闭长连接及其连接池
+			client.destroy();
+			client.getConnManager().destroy();
+		}
+	}
+	
+	@Before
+	public void before() {
+		String log4jFileStr = "log4j.properties";
+		PropertyConfigurator.configure(log4jFileStr);
+	}
+}
