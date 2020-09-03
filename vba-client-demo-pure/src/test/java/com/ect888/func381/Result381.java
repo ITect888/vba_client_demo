aac1eee50bf99d5c0eab8a2c4e6bd5b3d1167257
zhangwei <zhangwei@ect888.com>
2020-09-04 01:03:46 +0800

    feat：新增2000381出入境人像五要素核验

---
+++ vba-client-demo-pure/src/test/java/com/ect888/func381/Result381.java
@@ -0,0 +1,34 @@
+package com.ect888.func381;
+
+public class Result381 {
+	private String respinfo;
+	private String respcd;
+	private String sysseqno;
+	private String status;
+	
+	public String getRespinfo() {
+		return respinfo;
+	}
+	public void setRespinfo(String respinfo) {
+		this.respinfo = respinfo;
+	}
+	public String getRespcd() {
+		return respcd;
+	}
+	public void setRespcd(String respcd) {
+		this.respcd = respcd;
+	}
+	public String getSysseqno() {
+		return sysseqno;
+	}
+	public void setSysseqno(String sysseqno) {
+		this.sysseqno = sysseqno;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
